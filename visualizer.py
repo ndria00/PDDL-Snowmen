8078360df9f46e1606fdc01ae9cb16a2fe21885a
bfrancesco <brunofrancescobarbara@gmail.com>
2023-09-18 15:13:50 +0200

    added prints

--- visualizer.py
+++ visualizer.py
@@ -15,8 +15,9 @@ class Ball(Object) :
 class World:
     def __init__(self) -> None:
         self.player = None
-        self.map = None
+        self.map = []
         self.balls = {}
+        self.print_map={0:" ",1:"≡",2:"ጰ", 5:"◦", 6:"○", 7:"O", 8:"⦾", 9:"☃"}
 
     def read_map(self, text):
         rows = 0
@@ -36,12 +37,13 @@ class World:
                         rows = x1
                     if cols<y1:
                         cols = y1
-
-        self.map = [[0 for _ in range(cols)]]*rows
+        for i in range(0,rows):
+            self.map.append([0 for _ in range(cols)])
         for str in text:
+
             m = re.match(r'\s*\(\s*snow\s+loc\_(\d+)\_(\d+).+', str)
             if not (m is None):
-                    self.map[int(m.groups()[0])-1][int(m.groups()[1])-1] = 1
+                    self.map[int(m.groups()[1])-1][int(m.groups()[0])-1] += 1
 
             m = re.match(r'\s*\(\s*character_at\s+loc\_(\d+)\_(\d+).+', str)
             if not (m is None):
@@ -50,16 +52,32 @@ class World:
             m = re.match(r'\s*\(\s*ball_at\s+([a-zA-Z_0-9]+)\s*loc\_(\d+)\_(\d+).+', str)
             if not (m is None):
                    name = m.groups()[0]
-                   self.balls[name].x = int(m.groups()[1]) 
-                   self.balls[name].y = int(m.groups()[2]) 
-        for i in self.balls.keys():
-            print(f"{i} => {self.balls[i].x} - {self.balls[i].y}")
+                   self.balls[name].x = int(m.groups()[1]) -1
+                   self.balls[name].y = int(m.groups()[2]) -1
 
     def update_status(self, move):
         pass
 
     def print(self):
-        pass
+        copy_map = []
+        for i in self.map:
+            copy_map.append(i.copy())
+        copy_map[self.player.y][self.player.x] = 2
+        for i in self.balls.values():
+            if(copy_map[i.y][i.x]<5):
+                copy_map[i.y][i.x]=4+i.size
+            elif(copy_map[i.y][i.x]<8): 
+                copy_map[i.y][i.x]=8
+            else:
+                copy_map[i.y][i.x]=9
+
+        for i in range(0,len(copy_map)):
+            print("|",sep="",end="")
+            for j in copy_map[len(copy_map)-1-i]:
+               print(self.print_map[j],sep="",end="|")
+            print("",sep="")
+        print()
+
 
 if __name__== '__main__':
     if(len(sys.argv) != 3):
@@ -73,4 +91,7 @@ if __name__== '__main__':
     file.close()
     world = World()
     world.read_map(text)
-    #print(text)
+    with open(plan , "r") as planfile:
+        for line in planfile:
+            world.update_status(line)
+            world.print()
