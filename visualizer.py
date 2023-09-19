0c4ff4289ceb271421e701a7cc0a9adfe30183da
ndria00 <ctrndr00e01i872o@studenti.unical.it>
2023-09-19 12:07:47 +0200

    completed update_status method in visualizer

--- visualizer.py
+++ visualizer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import re 
 import sys
 
@@ -17,7 +18,7 @@ class World:
         self.player = None
         self.map = []
         self.balls = {}
-        self.print_map={0:" ",1:"≡",2:"ጰ", 5:"◦", 6:"○", 7:"O", 8:"⦾", 9:"☃"}
+        self.print_map={0:" ",1:"≡",2:"ጰ", 5:"◦", 6:"o", 7:"○", 8:"⦾", 9:"☃"}
 
     def read_map(self, text):
         rows = 0
@@ -56,7 +57,37 @@ class World:
                    self.balls[name].y = int(m.groups()[2]) -1
 
     def update_status(self, move):
-        pass
+        print(f"applying {move}")
+        m = re.match(r'.*\:\s*\(\s*move_character\s+loc\_(\d+)\_(\d+)\s+loc\_(\d+)\_(\d+).+', move) 
+        if not (m is None):
+            #print("Matched\n")
+            self.player = Object(int(m.groups()[2])-1, int(m.groups()[3])-1)
+        else:
+            #print("Not matched\")
+            m = re.match(r'.*\:\s*\(\s*move_ball\s+(ball_\d+)\s+loc\_(\d+)\_(\d+)\s+loc\_(\d+)\_(\d+)\s+loc\_(\d+)\_(\d+).+', move)
+            if not (m is None):
+                name = m.groups()[0]
+                x_to = int(m.groups()[5])-1
+                y_to = int(m.groups()[6])-1
+                x_from = int(m.groups()[3])-1
+                y_from = int(m.groups()[4])-1
+                
+                self.balls[name].x = x_to
+                self.balls[name].y = y_to
+                if self.map[y_to][x_to] == 1:
+                    if self.balls[name].size < 3:
+                        self.balls[name].size += 1 
+                count = 0
+                for b in self.balls.values():
+                        if b.y == y_from and b.x == x_from:
+                            count += 1
+                            
+                self.map[y_to][x_to] = 0
+                if count == 0:
+                    self.player.x = x_from
+                    self.player.y = y_from
+
+                
 
     def print(self):
         copy_map = []
@@ -91,6 +122,7 @@ if __name__== '__main__':
     file.close()
     world = World()
     world.read_map(text)
+    world.print()
     with open(plan , "r") as planfile:
         for line in planfile:
             world.update_status(line)
